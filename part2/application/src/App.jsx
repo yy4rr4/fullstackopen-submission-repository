c1d941386693276c974b3860d23f474ec9ac7574
yy4rr4 <yaroslavserhieiev2007@gmail.com>
2026-07-01 10:05:35 +0200

    Complete exercises up to 3.10 and prepare for Render deployment

--- part2/application/src/App.jsx
+++ part2/application/src/App.jsx
@@ -1,153 +1,60 @@
-// import { use, useEffect, useState } from "react";
-// import axios from "axios";
-// import Persons from "./components/Persons.jsx";
-// import Filter from "./components/Filter.jsx";
-// import PersonForm from "./components/PersonForm.jsx";
-// import Notification from "./components/Notification.jsx";
-// import noteServices from "./services/notes.js"
-
-// const App = () => {
-//   const [persons, setPersons] = useState([]);
-//   const [newName, setNewName] = useState("");
-//   const [newNumber, setNewNumber] = useState("");
-//   const [searchQuery, setSearchQuery] = useState("");
-//   const [successMessage, setSuccessMessage] = useState(null)
-//   const [errorMessage, setErrorMessage] = useState(null)
-
-//   useEffect(() => {
-//     noteServices.getAll().then((response) => {
-//       console.log(response.data);
-//       setPersons(response.data);
-//     });
-//   }, []);
-
-//   return (
-//     <div>
-//       <h2>Phonebook</h2>
-
-//       <Notification
-//         successMessage={successMessage}
-//         errorMessage={errorMessage}
-//       />
-
-//       <Filter searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-
-//       <h2>Add New</h2>
-
-//       <PersonForm
-//         persons={persons}
-//         setPersons={setPersons}
-//         newName={newName}
-//         setNewName={setNewName}
-//         newNumber={newNumber}
-//         setNewNumber={setNewNumber}
-//         setSuccessMessage={setSuccessMessage}
-//         setErrorMessage={setErrorMessage}
-//       />
-
-//       <h2>Numbers</h2>
-
-//       <Persons
-//         persons={persons}
-//         setPersons={setPersons}
-//         searchQuery={searchQuery}
-//         setSuccessMessage={setSuccessMessage}
-//         setErrorMessage={setErrorMessage}
-//       />
-//     </div>
-//   );
-// };
-
-// export default App;
-
-import axios from "axios";
 import { use, useEffect, useState } from "react";
+import axios from "axios";
+import Persons from "./components/Persons.jsx";
+import Filter from "./components/Filter.jsx";
+import PersonForm from "./components/PersonForm.jsx";
+import Notification from "./components/Notification.jsx";
+import noteServices from "./services/notes.js"
 
 const App = () => {
-
-  const api_key = import.meta.env.VITE_SOME_KEY;
-
-  const [countries, setCountries] = useState([]);
-  const [query, setQuery] = useState("");
-  const [weatherObject, setWeatherObject] = useState(null)
-
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [successMessage, setSuccessMessage] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+  
   useEffect(() => {
-    axios
-      .get("https://studies.cs.helsinki.fi/restcountries/api/all")
-      .then((response) => {
-        console.log("Countries data: ", response.data);
-        setCountries(response.data);
-      });
+    noteServices.getAll().then((response) => {
+      console.log(response.data);
+      setPersons(response.data);
+    });
   }, []);
-
-  const filteredCountries = countries.filter((country) =>
-    country.name.common.toLowerCase().includes(query.toLowerCase()),
-  );
-
-  useEffect(() => {
-    if (api_key && filteredCountries.length === 1) {
-      const targetCity = filteredCountries[0].capital[0];
-      console.log("Country: ", targetCity);
-      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${targetCity}&units=metric&appid=${api_key}`;
-
-      console.log("URL: ", weatherUrl);
-      console.log("API Key: ", api_key);
-
-      axios.get(weatherUrl).then((response) => {
-        console.log("Weather data: ", response.data);
-        setWeatherObject(response.data);
-      });
-    }
-  }, [query, filteredCountries])
-
-  const renderCountries = () => {
-
-    const showCountryInfo = (countryObj) => (
-      weatherObject && <div>
-        <h1>{countryObj.name.common}</h1>
-        <p>Capital {countryObj.capital[0]}</p>
-        <p>Area {countryObj.area}</p>
-        <h2>Languages</h2>
-        <ul>
-          {Object.values(countryObj.languages).map((language, id) => (
-            <li key={id}>{language}</li>
-          ))}
-        </ul>
-        <img src={countryObj.flags.png} />
-        <h2>Weather in {countryObj.name.common}</h2>
-        <p>Temperature {weatherObject.main.temp} Celsius</p>
-        <img
-          src={`https://openweathermap.org/img/wn/${weatherObject.weather[0].icon}@2x.png`}
-        />
-        <p>{weatherObject.wind.speed} m/s</p>
-      </div>
-    );
-
-    if (filteredCountries.length > 10 && query.length !== 0) {
-      return <p>Too many matches, specify another filter</p>;
-    }
-
-    if (filteredCountries.length === 1) {
-      return showCountryInfo(filteredCountries[0]);
-    }
-
-    return filteredCountries.map((country) => (
-      <p key={country.cca3}>
-        {country.name.common}{" "}
-        <button onClick={() => setQuery(country.name.common)}>Show</button>
-      </p>
-    ));
-  };
+  
 
   return (
     <div>
-      <form>
-        <p>
-          Find countries
-          <input value={query} onChange={(e) => setQuery(e.target.value)} />
-        </p>
-      </form>
-      {renderCountries()}
+      <h2>Phonebook</h2>
+
+      <Notification
+        successMessage={successMessage}
+        errorMessage={errorMessage}
+      />
+
+      <Filter searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+
+      <h2>Add New</h2>
+
+      <PersonForm
+        persons={persons}
+        setPersons={setPersons}
+        newName={newName}
+        setNewName={setNewName}
+        newNumber={newNumber}
+        setNewNumber={setNewNumber}
+        setSuccessMessage={setSuccessMessage}
+        setErrorMessage={setErrorMessage}
+      />
+
+      <h2>Numbers</h2>
+
+      <Persons
+        persons={persons}
+        setPersons={setPersons}
+        searchQuery={searchQuery}
+        setSuccessMessage={setSuccessMessage}
+        setErrorMessage={setErrorMessage}
+      />
     </div>
   );
 };
---
+++ part3/index.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require('cors')
+
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+
+morgan.token('body', (request) => {
+    return request.method === 'POST' ? JSON.stringify(request.body) : ''
+})
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
+);
+
+let phonebookData = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/persons", (request, response) => {
+  response.json(phonebookData);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = phonebookData.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.get("/info", (request, response) => {
+  const entreisCount = phonebookData.length;
+  const dateMessage = new Date();
+  response.send(`
+      <p>Phonebook has info about ${phonebookData.length} people</p>
+      <p>${dateMessage.toUTCString("en-US")}</p>
+    `);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  phonebookData = phonebookData.filter((person) => person.id != id);
+
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number missing"
+    });
+  }
+
+  const nameExists = phonebookData.some(
+    (person) => person.name.toLowerCase() === body.name.toLowerCase()
+  );
+
+  if (nameExists) {
+    return response.status(400).json({
+      error: "name must be unique"
+    });
+  }
+
+  const person = {
+    id: String(Math.floor(Math.random() * 1000)),
+    name: body.name,
+    number: body.number
+  };
+
+  phonebookData = phonebookData.concat(person);
+  response.json(phonebookData);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
